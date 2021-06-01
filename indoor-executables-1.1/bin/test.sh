86e0a5115a5cbfce8d06ebaa6f7f6917788f4ca7
Yixing Lao <yixing.lao@gmail.com>
2021-05-31 22:30:11 -0700

    add bin

---
+++ indoor-executables-1.1/bin/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pcl_kinfu_largeScale_release.exe
+GlobalRegistration.exe
+GraphOptimizer.exe
+BuildCorrespondence.exe
+FragmentOptimizer.exe
+Integrate.exe
+pcl_kinfu_largeScale_mesh_output_release.exe
+pcl_viewer_release.exe
