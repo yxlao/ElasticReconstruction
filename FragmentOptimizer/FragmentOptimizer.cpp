5b208d243c066a80a6fe208fbcdb6fd99e079cbc
Yixing Lao <yixing.lao@gmail.com>
2021-06-08 20:17:44 -0700

    tab to space

--- FragmentOptimizer/FragmentOptimizer.cpp
+++ FragmentOptimizer/FragmentOptimizer.cpp
@@ -10,29 +10,29 @@
 
 int print_help() {
     // clang-format off
-	cout << "\nApplication parameters:" << std::endl;
-	cout << "    --help, -h                      : print this message" << std::endl;
-	cout << "    --rgbdslam <log_file>           : rgbdslam.log/opt_output.log file, get ipose" << std::endl;
-	cout << "    --registration <log_file>       : reg_output.log, invalid pair when frame_ == -1" << std::endl;
-	cout << "    --dir <dir_prefix>              : dir prefix, place to loopup .xyzn files" << std::endl;
-	cout << "    --num <number>                  : number of pieces, important parameter" << std::endl;
-	cout << "    --weight <weight>               : 1.0 for nonrigid, 10000.0 for rigid" << std::endl;
-	cout << "    --resolution <resolution>       : default - 8" << std::endl;
-	cout << "    --length <length>               : default - 3.0" << std::endl;
-	cout << "    --interval <interval>           : default - 50" << std::endl;
-	cout << "    --iteration <max_number>        : default - 5" << std::endl;
-	cout << "    --inner_iteration <max_number>  : default - 10" << std::endl;
-	cout << "    --save_to <ctr_file>            : default - output.ctr" << std::endl;
-	cout << "    --write_xyzn_sample <sample_num>: per <sample_num> write a point" << std::endl;
-	cout << "    --blasklist <blacklist_file>    : each line is the block we want to blacklist" << std::endl;
-	cout << "    --blacklistpair <threshold>     : threshold of accepting pairwise registration, default - 10000" << std::endl;
-	cout << "    --switchable <weight>           : use switchable g2o, default weight 0.1" << std::endl;
-	cout << "    --ipose <log_file>              : get ipose from log file" << std::endl;
-	cout << "    --verbose <verbpse_level>       : set verbosity level, from 0(none) to 5(verbose), default 3" << std::endl;
-	cout << "Optimization options:" << std::endl;
-	cout << "    --nonrigid                      : default, nonrigid alignment published in ICCV 2013" << std::endl;
-	cout << "    --rigid                         : dense rigid optimization" << std::endl;
-	cout << "    --slac                          : simultaneous localization and calibration, published in CVPR 2014" << std::endl;
+    std::cout << "\nApplication parameters:" << std::endl;
+    std::cout << "    --help, -h                      : print this message" << std::endl;
+    std::cout << "    --rgbdslam <log_file>           : rgbdslam.log/opt_output.log file, get ipose" << std::endl;
+    std::cout << "    --registration <log_file>       : reg_output.log, invalid pair when frame_ == -1" << std::endl;
+    std::cout << "    --dir <dir_prefix>              : dir prefix, place to loopup .xyzn files" << std::endl;
+    std::cout << "    --num <number>                  : number of pieces, important parameter" << std::endl;
+    std::cout << "    --weight <weight>               : 1.0 for nonrigid, 10000.0 for rigid" << std::endl;
+    std::cout << "    --resolution <resolution>       : default - 8" << std::endl;
+    std::cout << "    --length <length>               : default - 3.0" << std::endl;
+    std::cout << "    --interval <interval>           : default - 50" << std::endl;
+    std::cout << "    --iteration <max_number>        : default - 5" << std::endl;
+    std::cout << "    --inner_iteration <max_number>  : default - 10" << std::endl;
+    std::cout << "    --save_to <ctr_file>            : default - output.ctr" << std::endl;
+    std::cout << "    --write_xyzn_sample <sample_num>: per <sample_num> write a point" << std::endl;
+    std::cout << "    --blasklist <blacklist_file>    : each line is the block we want to blacklist" << std::endl;
+    std::cout << "    --blacklistpair <threshold>     : threshold of accepting pairwise registration, default - 10000" << std::endl;
+    std::cout << "    --switchable <weight>           : use switchable g2o, default weight 0.1" << std::endl;
+    std::cout << "    --ipose <log_file>              : get ipose from log file" << std::endl;
+    std::cout << "    --verbose <verbpse_level>       : set verbosity level, from 0(none) to 5(verbose), default 3" << std::endl;
+    std::cout << "Optimization options:" << std::endl;
+    std::cout << "    --nonrigid                      : default, nonrigid alignment published in ICCV 2013" << std::endl;
+    std::cout << "    --rigid                         : dense rigid optimization" << std::endl;
+    std::cout << "    --slac                          : simultaneous localization and calibration, published in CVPR 2014" << std::endl;
     // clang-format on
     return 0;
 }
