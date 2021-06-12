63304b4c617fda9368ffb2a18ce95f90d5bb12b7
Yixing Lao <yixing.lao@gmail.com>
2021-06-12 02:29:31 -0700

    bin dir

--- Scripts/setup_env.sh
+++ Scripts/setup_env.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Source this file to set up the environment.
 
 set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-PCL_BUILD_DIR=${SCRIPT_DIR}/../../StanfordPCL/build
+BIN_DIR=`realpath ${SCRIPT_DIR}/../build/bin`
+PCL_BIN_DIR=`realpath ${SCRIPT_DIR}/../../StanfordPCL/build/bin`
+echo "BIN_DIR: ${BIN_DIR}"
+echo "PCL_BIN_DIR: ${PCL_BIN_DIR}"
 
-export pcl_kinfu_largeScale_release=${PCL_BUILD_DIR}/bin/pcl_kinfu_largeScale
+export pcl_kinfu_largeScale_release=${PCL_BIN_DIR}/pcl_kinfu_largeScale
 echo "pcl_kinfu_largeScale_release:"
 ls -alh ${pcl_kinfu_largeScale_release}
 
@@ -17,7 +20,3 @@ ls -alh ${pcl_kinfu_largeScale_release}
 # Integrate.exe
 # pcl_kinfu_largeScale_mesh_output_release.exe
 # pcl_viewer_release.exe
-
-
-echo "pcl_kinfu_largeScale_release:"
-ls -alh ${pcl_kinfu_largeScale_release}
