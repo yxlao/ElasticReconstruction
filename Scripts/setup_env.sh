ad0c415283325d71b3d7d5406e2ea26c549d6012
Yixing Lao <yixing.lao@gmail.com>
2021-06-12 02:33:29 -0700

    export all paths

--- Scripts/setup_env.sh
+++ Scripts/setup_env.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Source this file to set up the environment.
+# pcl_viewer() is not supported
 
 set -eu
 
@@ -9,14 +10,18 @@ PCL_BIN_DIR=`realpath ${SCRIPT_DIR}/../../StanfordPCL/build/bin`
 echo "BIN_DIR: ${BIN_DIR}"
 echo "PCL_BIN_DIR: ${PCL_BIN_DIR}"
 
-export pcl_kinfu_largeScale_release=${PCL_BIN_DIR}/pcl_kinfu_largeScale
-echo "pcl_kinfu_largeScale_release:"
-ls -alh ${pcl_kinfu_largeScale_release}
+export pcl_kinfu_largeScale=${PCL_BIN_DIR}/pcl_kinfu_largeScale
+export GlobalRegistration=${BIN_DIR}/GlobalRegistration
+export GraphOptimizer=${BIN_DIR}/GraphOptimizer
+export BuildCorrespondence=${BIN_DIR}/BuildCorrespondence
+export FragmentOptimizer=${BIN_DIR}/FragmentOptimizer
+export Integrate=${BIN_DIR}/Integrate
+export pcl_kinfu_largeScale_mesh_output=${PCL_BIN_DIR}/pcl_kinfu_largeScale_mesh_output
 
-# GlobalRegistration.exe
-# GraphOptimizer.exe
-# BuildCorrespondence.exe
-# FragmentOptimizer.exe
-# Integrate.exe
-# pcl_kinfu_largeScale_mesh_output_release.exe
-# pcl_viewer_release.exe
+echo "# pcl_kinfu_largeScale:"             && ls -alh ${pcl_kinfu_largeScale}
+echo "# GlobalRegistration:"               && ls -alh ${GlobalRegistration}
+echo "# GraphOptimizer:"                   && ls -alh ${GraphOptimizer}
+echo "# BuildCorrespondence:"              && ls -alh ${BuildCorrespondence}
+echo "# FragmentOptimizer:"                && ls -alh ${FragmentOptimizer}
+echo "# Integrate:"                        && ls -alh ${Integrate}
+echo "# pcl_kinfu_largeScale_mesh_output:" && ls -alh ${pcl_kinfu_largeScale_mesh_output}
