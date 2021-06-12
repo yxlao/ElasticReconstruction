3dfd7f9fc1c55fe4cbfb3cf4daef8b7d1c317cad
Yixing Lao <yixing.lao@gmail.com>
2021-06-12 02:37:37 -0700

    Sandbox

--- Scripts/demo.sh
+++ Scripts/demo.sh
@@ -1,33 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-pcl_kinfu_largeScale_release.exe -r -ic -sd 10 -oni ../sandbox/input.oni -vs 4 --fragment 25 --rgbd_odometry --record_log ../sandbox/100-0.log --camera longrange.param
-mkdir ../sandbox/pcds/
-mv cloud_bin* ../sandbox/pcds/
+set -eu
 
-GlobalRegistration.exe ../sandbox/pcds/ ../sandbox/100-0.log 50
-mv init.log ../sandbox/
-mv pose.log ../sandbox/
-mv odometry.* ../sandbox/
-mv result.* ../sandbox/
+${pcl_kinfu_largeScale} -r -ic -sd 10 -oni ../Sandbox/input.oni -vs 4 --fragment 25 --rgbd_odometry --record_log ../Sandbox/100-0.log --camera longrange.param
+mkdir ../Sandbox/pcds/
+mv cloud_bin* ../Sandbox/pcds/
 
-GraphOptimizer.exe -w 100 --odometry ../sandbox/odometry.log --odometryinfo ../sandbox/odometry.info --loop ../sandbox/result.txt --loopinfo ../sandbox/result.info --pose ../sandbox/pose.log --keep ../sandbox/keep.log --refine ../sandbox/pcds/reg_refine_all.log
+GlobalRegistration.exe ../Sandbox/pcds/ ../Sandbox/100-0.log 50
+mv init.log ../Sandbox/
+mv pose.log ../Sandbox/
+mv odometry.* ../Sandbox/
+mv result.* ../Sandbox/
 
-BuildCorrespondence.exe --reg_traj ../sandbox/pcds/reg_refine_all.log --registration --reg_dist 0.05 --reg_ratio 0.25 --reg_num 0 --save_xyzn
-mv reg_output.* ../sandbox/
+GraphOptimizer.exe -w 100 --odometry ../Sandbox/odometry.log --odometryinfo ../Sandbox/odometry.info --loop ../Sandbox/result.txt --loopinfo ../Sandbox/result.info --pose ../Sandbox/pose.log --keep ../Sandbox/keep.log --refine ../Sandbox/pcds/reg_refine_all.log
 
-numpcds=$(ls ../sandbox/pcds/cloud_bin_*.pcd -l | wc -l | tr -d ' ')
-#FragmentOptimizer.exe --rigid --rgbdslam ../sandbox/init.log --registration ../sandbox/reg_output.log --dir ../sandbox/pcds/ --num $numpcds --resolution 12 --iteration 10 --length 4.0 --write_xyzn_sample 10
-FragmentOptimizer.exe --slac --rgbdslam ../sandbox/init.log --registration ../sandbox/reg_output.log --dir ../sandbox/pcds/ --num $numpcds --resolution 12 --iteration 10 --length 4.0 --write_xyzn_sample 10
+BuildCorrespondence.exe --reg_traj ../Sandbox/pcds/reg_refine_all.log --registration --reg_dist 0.05 --reg_ratio 0.25 --reg_num 0 --save_xyzn
+mv reg_output.* ../Sandbox/
+
+numpcds=$(ls ../Sandbox/pcds/cloud_bin_*.pcd -l | wc -l | tr -d ' ')
+#FragmentOptimizer.exe --rigid --rgbdslam ../Sandbox/init.log --registration ../Sandbox/reg_output.log --dir ../Sandbox/pcds/ --num $numpcds --resolution 12 --iteration 10 --length 4.0 --write_xyzn_sample 10
+FragmentOptimizer.exe --slac --rgbdslam ../Sandbox/init.log --registration ../Sandbox/reg_output.log --dir ../Sandbox/pcds/ --num $numpcds --resolution 12 --iteration 10 --length 4.0 --write_xyzn_sample 10
 #pcl_viewer_release.exe sample.pcd
-mv pose.log ../sandbox/pose_slac.log
-mv output.ctr ../sandbox/
+mv pose.log ../Sandbox/pose_slac.log
+mv output.ctr ../Sandbox/
 rm sample.pcd
 
-Integrate.exe --pose_traj ../sandbox/pose_slac.log --seg_traj ../sandbox/100-0.log --ctr ../sandbox/output.ctr --num ${numpcds} --resolution 12 --camera longrange.param -oni ../sandbox/input.oni --length 4.0 --interval 50
+Integrate.exe --pose_traj ../Sandbox/pose_slac.log --seg_traj ../Sandbox/100-0.log --ctr ../Sandbox/output.ctr --num ${numpcds} --resolution 12 --camera longrange.param -oni ../Sandbox/input.oni --length 4.0 --interval 50
 
 pcl_kinfu_largeScale_mesh_output_release.exe world.pcd
-mkdir ../sandbox/ply/
-mv *.ply ../sandbox/ply/
+mkdir ../Sandbox/ply/
+mv *.ply ../Sandbox/ply/
 rm world.pcd
 
 # MeshLab -> import all ply files -> merge visible layers -> make sure "remove duplicated vertices/faces" are checked -> export final model
