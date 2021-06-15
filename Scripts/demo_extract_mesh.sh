2ef994143a8d76650f3f903e4ce8155cb92713cf
Yixing Lao <yixing.lao@gmail.com>
2021-06-15 04:13:13 -0700

    add demo extract mesh

---
+++ Scripts/demo_extract_mesh.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eu
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source ${SCRIPT_DIR}/setup_env.sh
+
+# Number of CPU cores, not counting hyper-threading:
+# https://stackoverflow.com/a/6481016/1255535
+#
+# Typically, set max # of threads to the # of physical cores, not logical:
+# https://www.thunderheadeng.com/2014/08/openmp-benchmarks/
+# https://stackoverflow.com/a/36959375/1255535
+NUM_CORES=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
+export OMP_NUM_THREADS=${NUM_CORES}
+echo "OMP_NUM_THREADS: ${OMP_NUM_THREADS}"
+
+# Part V: SLAC (or rigid) optimization
+NUM_PCDS=$(ls ../Sandbox/pcds/cloud_bin_*.pcd -l | wc -l | tr -d ' ')
+
+# Part VII: Extract mesh
+${pcl_kinfu_largeScale_mesh_output} world.pcd
+mkdir ../Sandbox/ply/
+mv *.ply ../Sandbox/ply/
