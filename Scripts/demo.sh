21608b5320a7ccc19c340d8314cb21169221e2ca
Yixing Lao <yixing.lao@gmail.com>
2021-06-18 02:32:09 -0700

    annotate input and output for each step

--- Scripts/demo.sh
+++ Scripts/demo.sh
@@ -15,6 +15,12 @@ export OMP_NUM_THREADS=${NUM_CORES}
 echo "OMP_NUM_THREADS: ${OMP_NUM_THREADS}"
 
 # Part I: Create fragments
+# Inputs:
+#     Sandbox/input.oni
+#     Scripts/longrange.param
+# Outputs:
+#     Sandbox/pcds/cloud_bin_xxx.pcd * 57
+#     Sandbox/100-0.log                             # 08∶39∶11 PM PDT
 ${pcl_kinfu_largeScale} -r -ic -sd 10 -oni ../Sandbox/input.oni -vs 4 \
     --fragment 25 --rgbd_odometry --record_log ../Sandbox/100-0.log \
     --camera longrange.param
@@ -22,6 +28,16 @@ mkdir -p ../Sandbox/pcds/
 mv cloud_bin* ../Sandbox/pcds/
 
 # Part II: Global registration
+# Inputs:
+#     Sandbox/pcds/cloud_bin_xxx.pcd * 57
+#     Sandbox/100-0.log
+# Outputs:
+#     Sandbox/init.log      # create_init_traj()    # 08∶39∶11 PM PDT
+#     Sandbox/odometry.info # create_odometry()     # 08∶39∶22 PM PDT
+#     Sandbox/odometry.log  # create_odometry()     # 08∶39∶22 PM PDT
+#     Sandbox/result.txt    # do_all()              # 01∶09∶55 AM PDT
+#     Sandbox/result.info   # do_all()              # 01∶09∶55 AM PDT
+#     Sandbox/pose.log      # create_pose_traj()    # 01∶09∶55 AM PDT
 ${GlobalRegistration} ../Sandbox/pcds/ ../Sandbox/100-0.log 50
 mv init.log ../Sandbox/
 mv pose.log ../Sandbox/
@@ -29,6 +45,18 @@ mv odometry.* ../Sandbox/
 mv result.* ../Sandbox/
 
 # Part III: Graph optimization
+# Inputs:
+#     Sandbox/init.log
+#     Sandbox/odometry.info
+#     Sandbox/odometry.log
+#     Sandbox/result.txt
+#     Sandbox/result.info
+#     Sandbox/pose.log
+#     (no need to use Sandbox/pcds/cloud_bin_xxx.pcd, no `loadPCDFile`)
+# Outputs:
+#     Sandbox/keep.log                          # 01∶09∶55 AM PDT
+#     Sandbox/pcds/reg_refine_all.log           # 01∶09∶55 AM PDT
+loop_remain_log_file_
 ${GraphOptimizer} -w 100 \
     --odometry ../Sandbox/odometry.log \
     --odometryinfo ../Sandbox/odometry.info \
@@ -39,6 +67,14 @@ ${GraphOptimizer} -w 100 \
     --refine ../Sandbox/pcds/reg_refine_all.log
 
 # Part IV: Build correspondence
+# Inputs:
+#     Sandbox/pcds/cloud_bin_xxx.pcd
+#     Sandbox/pcds/reg_refine_all.log
+# Outputs:
+#     Sandbox/pcds/cloud_bin_xyzn_xx.xyzn * 57  # 01∶09∶56 AM PDT
+#     Sandbox/pcds/corres_xx_xx.txt       * 221 # 01∶46∶40 AM PDT (the last one)
+#     Sandbox/reg_output.log                    # 01∶46∶35 AM PDT
+#     Sandbox/reg_output.info (optional)
 ${BuildCorrespondence} \
     --reg_traj ../Sandbox/pcds/reg_refine_all.log \
     --registration \
@@ -49,6 +85,15 @@ ${BuildCorrespondence} \
 mv reg_output.* ../Sandbox/
 
 # Part V: SLAC (or rigid) optimization
+# Inputs:
+#     Sandbox/pcds/cloud_bin_xxx.pcd * 57
+#     Sandbox/pcds/corres_xx_xx.txt  * 221
+#     Sandbox/init.log
+#     Sandbox/reg_output.log
+# Outputs:
+#     Sandbox/pose_slac.log                     # 01∶52∶21 AM PDT
+#     Sandbox/output.ctr                        # 01∶52∶21 AM PDT
+#     Sandbox/sample.pcd                        # 01∶52∶22 AM PDT
 NUM_PCDS=$(ls ../Sandbox/pcds/cloud_bin_*.pcd -l | wc -l | tr -d ' ')
 # ${FragmentOptimizer} --rigid \
 #     --rgbdslam ../Sandbox/init.log \
@@ -70,10 +115,17 @@ ${FragmentOptimizer} --slac \
     --write_xyzn_sample 10
 mv pose.log ../Sandbox/pose_slac.log
 mv output.ctr ../Sandbox/
-# pcl_viewer_release.exe sample.pcd
-# rm sample.pcd
+mv sample.pcd ../Sandbox/
 
 # Part VI: Integration
+# Inputs:
+#     Sandbox/pose_slac.log
+#     Sandbox/100-0.log
+#     Sandbox/output.ctr
+#     Sandbox/input.oni
+#     Scripts/longrange.param
+# Outputs:
+#     Scripts/world.pcd
 ${Integrate} --pose_traj ../Sandbox/pose_slac.log \
     --seg_traj ../Sandbox/100-0.log \
     --ctr ../Sandbox/output.ctr \
@@ -85,10 +137,14 @@ ${Integrate} --pose_traj ../Sandbox/pose_slac.log \
     --interval 50
 
 # Part VII: Extract mesh
+# Inputs:
+#     Scripts/world.pcd
+# Outputs:
+#     Sandbox/ply/mesh_xx.ply * 7
 ${pcl_kinfu_largeScale_mesh_output} world.pcd -vs 4
 mkdir ../Sandbox/ply/
 mv *.ply ../Sandbox/ply/
-# rm world.pcd
+mv world.pcd ../Sandbox/
 
 # Part VIII: Visualization
 # MeshLab
@@ -96,3 +152,4 @@ mv *.ply ../Sandbox/ply/
 #     -> merge visible layers
 #     -> make sure "remove duplicated vertices/faces" are checked
 #     -> export final model
+python view_results.py
